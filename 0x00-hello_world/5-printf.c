e1fb0f419012e50f27d39ce4b875abad8d3e222e
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-06-30 06:00:49 -0700

    program that prints exactly

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+
+/* program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line. */
+
+int main(viod)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,2 +1,3 @@
 C programming Hello World readme
 1_script that compiles a C file but does not link
+4_C program that prints exactly "Programming is like building a multilingual puzzle
