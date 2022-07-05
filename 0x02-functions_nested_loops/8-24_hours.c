e9d689cee359b04709c8e1457d4f3a4f90ab52f3
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-07-05 16:15:00 -0700

    print all minutes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,3 +1,4 @@
+#include <std.h>
 #include "main.h"
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
@@ -6,4 +7,17 @@
  */
 void jack_bauer(void)
 {
+	int h, m;
 
+	for (h = 0; h <= 23; h++)
+	{
+		printf("%2d :", h);
+		for (m = 0; m <= 59; m++)
+		{
+			printf("%2d", m);
+			m++;
+		}
+		h++;
+	}
+	printf("\n");
+}
