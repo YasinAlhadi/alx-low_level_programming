c051d5e36ec8366a4cb58d4e1d08b454f1b0c04d
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-07-06 16:04:26 -0700

    prints the sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <std.h>
+/*
+ * main - computes and prints the sum of all the multiples of 3 or 5
+ *
+ * Description: program compute and print the sum
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n, sum;
+
+	sum = 0;
+
+	for (n = 0; n < 1025; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			sum = sum + n;
+		}
+	}
+	printf("%d", sum);
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -11,3 +11,4 @@
 8_  function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
 9_ function that prints the 9 times table, starting with 0.
 11_  function that prints all natural numbers from n to 98, followed by a new line
+13_ program that computes and prints the sum of all the multiples of 3 or 5 below 1024.
