ee5a35c9cd3734c3ab30d48e9c8855560ff55f4e
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-07-08 11:59:12 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,24 +13,16 @@ int main(void)
 	for (n = 1; n < 101; n++)
 	{
 		if (n % 5 == 0 && n % 3 == 0)
-		{
 			printf("FizzBuzz ");
-		}
 		else if (n % 5 == 0)
-		{
 			if (n == 100)
 				printf("Buzz");
 			else
 				printf("Buzz ");
-		}
 		else if (n % 3 == 0)
-		{
 			printf("Fizz ");
-		}
 		else
-		{
 			printf("%d", n);
-		}
 	}
 	printf("\n");
 	return (0);
