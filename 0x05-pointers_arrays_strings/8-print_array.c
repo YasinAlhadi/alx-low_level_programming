92a11608b07e65370f426a3b7c7e6eb901c826ea
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-07-11 16:30:43 -0700

    print array elemant

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,14 +11,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	i = 0;
-	while (a[i] != '\0' )
+	for (i = 0; i < n; i++)
 	{
-		if (i == n -1)
-		{
-			printf("%d, ", a[i]);
-		}
-		i++;
+		printf("%d", a[i]);
+		if (i == n - 1)
+			continue;
+		printf(", ")
 	}
-		printf("%d\n", a[i]);
+		printf("\n");
 }
