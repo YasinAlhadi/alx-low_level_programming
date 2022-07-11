f8538ab7dc0ba7879a686fb2f20894652151d5d8
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-07-11 12:06:01 -0700

    reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,14 +9,14 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (str[i] != '\0')
 	{
 		i++;
-	
 	}
-	while (s[i] >= 0)
+	i--;
+	while (str[i] >= 0)
 	{
-		_putchar(s[i]);
+		_putchar(str[i]);
 		i--;
 	}
 	_putchar('\n');
