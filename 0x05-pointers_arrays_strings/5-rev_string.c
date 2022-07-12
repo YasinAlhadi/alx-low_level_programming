264c2670c52b3ad229857257025297ac9d05a1f7
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-07-11 18:16:36 -0700

    reverses a string.

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,4 +7,19 @@
  */
 void rev_string(char *s)
 {
-	int i = 0;
+	int i, y, z;
+	char a;
+
+	i = 0;
+	z = 0;
+	while (s[i] != '\0')
+		i++;
+	z = i - 1;
+	for (y = 0; y < i / 2; y++)
+	{
+		a = s[y];
+		s[y] = s[z];
+		s[z] = a;
+		z -= 1;
+	}
+}
