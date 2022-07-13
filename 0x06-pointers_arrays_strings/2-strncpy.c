2cd557d7fe977c50034d695f037bc916a5dfc1e6
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-07-13 08:39:49 -0700

    copies strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,15 +4,21 @@
  * @dest: first string
  * @src: second srting
  * @n: number of bytes
- * Return: char
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n);
 {
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
 		dest[i] = src[i];
-	dest[i] = '\0';
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 
 	return (dest);
 }
