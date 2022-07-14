657569f0941ed55420c73a187a6e7716a48902f3
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-07-14 01:10:30 -0700

    capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,26 +7,25 @@
  */
 char *cap_string(char *s)
 {
-	int i, w;
+	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		w = i - 1;
 		if (s[i] >= 'a' && s[i] <= 'z')
 		{
 			if (i == 0)
 				s[i] = s[i] - 32;
-			else if (s[w] >= 9 && s[w] <= 10)
+			else if (s[i - 1] >= 9 && s[i - 1] <= 10)
 				s[i] = s[i] - 32;
-			else if (s[w] >= 32 && s[w] <= 34)
+			else if (s[i - 1] >= 32 && s[i - 1] <= 34)
 				s[i] = s[i] - 32;
-			else if (s[w] >= 40 && s[w] <= 41)
+			else if (s[i - 1] >= 40 && s[i - 1] <= 41)
 				s[i] = s[i] - 32;
-			else if (s[w] == 46)
+			else if (s[i - 1] == 46)
 				s[i] = s[i] - 32;
-			else if (s[w] == 59)
+			else if (s[i - 1] == 59)
 				s[i] = s[i] - 32;
-			else if (s[w] >= 123 || s[w] <= 125)
+			else if (s[i - 1] >= 123 || s[i - 1] <= 125)
 				s[i] = s[i] - 32;
 		}
 	}
