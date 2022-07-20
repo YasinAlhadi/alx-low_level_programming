698f3de878fde7b99063c30eb7b96a0b586b882f
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-07-20 04:18:02 -0700

    x power y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,11 +8,7 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
-	{
-		return (-1);
-	}
-	else if (y == 0)
+	if (y == 0)
 	{
 		return (1);
 	}
@@ -20,5 +16,7 @@ int _pow_recursion(int x, int y)
 	{
 		return (x * _pow_recursion(x, y - 1));
 	}
+	else
+	return(-1);
 }
 
