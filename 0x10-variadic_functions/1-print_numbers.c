ee65418ce28f1c43b0d6e753908fa834a6f0d8d4
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-08-04 02:11:41 -0700

    returns the sum of all its parameters

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints numbers
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ *
+ * Return: Nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	va_start(args, n);
+	unsigned int i;
+
+	for(i = 0; i < n; i++)
+	{
+		int num = va_arg(args, unsigned int);
+		if (separator == NULL)
+		{
+			if (i == n - 1)
+				printf("%d\n", num);
+			else
+				printf("%d", num);
+		}
+		else
+		{
+			if (i == n - 1)
+				printf("%d\n", num);
+			else
+				printf("%d%s", num, separator);
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,5 @@
+# C - Variadic functions
+0_  function that returns the sum of all its parameters.<br />
+1_ function that prints numbers, followed by a new line.<br />
+2_ function that prints strings, followed by a new line.<br />
+3_ function that prints anything.
