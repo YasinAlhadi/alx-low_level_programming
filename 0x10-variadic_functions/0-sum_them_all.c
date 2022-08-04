9553d2a57adc47c197bf5f0f3d9a81e7f111e642
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-08-04 02:49:40 -0700

    print sum of elements

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,12 +11,14 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
 
-	va_start(args, n);
-	int sum = 0;
+	int sum;
+	unsigned int i;
 
 	if (n == 0)
 		return (0);
-	for (unsigned int i = 0; i < n; i++)
+	va_start(args, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
 	}
