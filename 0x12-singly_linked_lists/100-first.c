ecd61c046a732c183d544d03a797f3479b5700f6
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-08-11 12:29:18 -0700

    function that first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * before_main_execute - function that execute first
+ */
+void __attribute__ ((constructor)) before_main_execute()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
