f7cc50653ce116e056c8c0040216c3aadc368973
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-08-12 05:14:21 -0700

    fix betty style for header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,12 +6,12 @@
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
- * len: length of the string
+ * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  */
-typedef struct list_s 
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
