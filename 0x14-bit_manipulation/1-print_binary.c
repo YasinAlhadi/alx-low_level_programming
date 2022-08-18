66e6dfe3b1ec06a592f903c6dd9b13137aa24a5a
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-08-18 07:39:49 -0700

    prints the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,7 +7,18 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n / 2);
-	_putchar(n % 2);
+	int i, check;
+	unsigned int a;
+
+	check = 0;
+	if (n == 0)
+		_putchar('0');
+	for (i = 63; i >= 0; i--)
+	{
+		a = (n >> i) & 1;
+		if (a == 1)
+			check = 1;
+		if (check == 1)
+			_putchar(((n >> i) & 1) + '0');
+	}
 }
