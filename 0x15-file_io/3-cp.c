6573ca3f6016e612442a76846fd29c3fe7168acb
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-08-22 14:54:17 -0700

    copies the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,65 @@
+#include "main.h"
+/**
+ * error97 - check correct number of argument
+ * @argc: number of argument
+ *
+ * Return: void
+ */
+void error97(int argc)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+}
+/**
+ * main - copies the content of a file to another file
+ * @argc: number of argument
+ * @argv: array of argument
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int f_from, f_to;
+	ssize_t rbytes, wbytes;
+	char buffer[1024];
+
+	error97(argc);
+	f_from = open(argv[1], O_RDONLY);
+	if (f_from == -1)
+	{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
+	}
+	f_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (f_to == -1)
+	{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+	while ((rbytes = read(f_from, buffer, 1024)) > 0)
+	{
+		wbytes = write(f_to, buffer, rbytes);
+		if (wbytes == -1)
+		{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+		}
+	}
+	if (rbytes == -1)
+	{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
+	}
+	else if (close(f_from) == -1)
+	{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_from), exit(98);
+	}
+	else if (close(f_to) == -1)
+	{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_to);
+	exit(100);
+	}
+	return (0);
+}
